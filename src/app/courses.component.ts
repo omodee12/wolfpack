0218805226beb7bffb6b3a5d9fd385d004c6a2f5
unknown <omodara.tobi@gmail.com>
2021-07-01 15:29:44 +0100

    :zap:

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -7,13 +7,24 @@ import { CoursesService } from "./courses.service";
         <ul>
         <li *ngFor="let course of courses">
         {{ course }}
-        </li></ul>
+        </li>
+        </ul>
+        <img src="{{ imageUrl }}"/>
+        <table>
+        <tr>
+        <td [attr.colspan]="colSpan"></td>
+        </tr>
+        </table>
+        <button class="btn btn-success" [class.active]="isActive">Save</button>
     `
 })
 
 export class CoursesComponent{
     title = 'Lists of courses';
     courses;
+    imageUrl = "https://unsplash.com/photos/MqT0asuoIcU/400/200";
+    colSpan = 2;
+    isActive = true;
 
     getTitle(){
         return this.title;
@@ -22,4 +33,5 @@ export class CoursesComponent{
     constructor(service: CoursesService){
         this.courses = service.getCourses();
     }
+    
 }
