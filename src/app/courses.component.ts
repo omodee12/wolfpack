3e19de43ac1d434ca6cfbf173e6e16833816137f
unknown <omodara.tobi@gmail.com>
2021-07-02 14:54:57 +0100

    :zap:

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,24 +4,33 @@ import { CoursesService } from "./courses.service";
 @Component({
     selector: 'courses',
     template: `<h2>{{ getTitle() }}</h2>
-        <ul>
-        <li *ngFor="let course of courses">
-        {{ course }}
-        </li>
-        </ul>
+        {{ courses.title | uppercase }} <br/>
+        {{ courses.students }} <br/>
+        {{ courses.rating | number }} <br/>
+        {{ courses.price }} <br/>
+        {{ courses.releaseDate }} <br/>
+
         <img src="{{ imageUrl }}"/>
         <table>
         <tr>
         <td [attr.colspan]="colSpan"></td>
         </tr>
         </table>
-        <button class="btn btn-success" [class.active]="isActive">Save</button>
+        <button class="btn btn-success" (click)="onSave($event)">Save</button>
+        <div><input (keyup.enter)="onKeyUp($event)"/></div>
+        
     `
 })
 
 export class CoursesComponent{
     title = 'Lists of courses';
-    courses;
+    courses = {
+        title: 'book',
+        students: '345',
+        rating: '4.2',
+        price: '100.95',
+        releaseDate: new Date(2021, 6,  2)
+    };
     imageUrl = "https://unsplash.com/photos/MqT0asuoIcU/400/200";
     colSpan = 2;
     isActive = true;
@@ -29,9 +38,15 @@ export class CoursesComponent{
     getTitle(){
         return this.title;
     }
-
-    constructor(service: CoursesService){
-        this.courses = service.getCourses();
+    onSave($event:any){
+        console.log('button was clicked', $event);
     }
+    onKeyUp($event:any){
+        console.log($event.target.value);
+    }
+
+    // constructor(service: CoursesService){
+    //     this.courses = service.getCourses();
+    // }
     
 }
